fe14e21d0da54eb212199464248b5601c99ae8be
dau minh tuan <dautuan032004@gmai.com>
2024-11-12 13:48:24 +0700

    change1

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view/>
+</template>
+<script setup lang="ts">
+import router from './router'
+const token = localStorage.getItem('jwt');
+if(token == null){
+  router.push('./QuanLySach')
+}
+else router.push('./');
+</script>
