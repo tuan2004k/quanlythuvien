55074a228254e22179cf4ede08a959d9d779a9b8
dau minh tuan <dautuan032004@gmai.com>
2024-11-16 14:32:40 +0700

    thay doi

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -103,7 +103,6 @@ export default {
       roles: ["Độc giả", "Thủ thư", "Quản trị viên"],
     };
   },
-  console.log("Test")
   methods: {
     submitForm() {
       const savedUser = JSON.parse(localStorage.getItem("newUser"));
